539a0846e7fb8ae0c0854d323a3c68c515ff184d
aadhyaroy44-create <aadhyaroy44@gmail.com>
2026-09-24 14:31:13 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 
 
 # Configure Gemini API
-genai.configure(api_key="AQ.Ab8RN6L-qwFh-97Ee5QnUBm1fkIa7btPTN0v3oE9fwXY4LXUug")
+genai.configure(api_key="AQ.Ab8RN6JJxLRDLvUQM5SI8WjMn0g5AK77m6j5UDz6XA0eIjnUZA")
 
 
 model = genai.GenerativeModel("gemini-flash-latest")
